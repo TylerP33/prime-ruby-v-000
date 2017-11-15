6fd88b9fc957f5777bad6014c1e13bb991ee18ca
Tyler Pulse <tyler3321@gmail.com>
2017-11-15 04:43:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+num = (0..1000).to_a
+  num.select {|value|
+    if value % (value + 1) == 0
+      return true
+    else
+      value % 2 == 0 || value != (value - 1)
+      return false
+  end
+}
+end
